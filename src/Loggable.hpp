351bfc79a73f72924027f8aa641a3d51d594a73e
zmertens <zach.mm035@gmail.com>
2026-03-01 20:39:12 -0700

    checkpt

--- src/Loggable.hpp
+++ src/Loggable.hpp
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#ifndef LOGGABLE_HPP
-#define LOGGABLE_HPP
-
-#include <string_view>
-
-class Loggable
-{
-public:
-    virtual ~Loggable() = default;
-    virtual void log(std::string_view message, const char delimiter = '\n') noexcept = 0;
-    virtual std::string_view view() const noexcept = 0;
-    virtual std::string_view consumeView() noexcept = 0;
-};
-
-#endif // LOGGABLE_HPP
-=======
 #ifndef LOGGABLE_HPP
 #define LOGGABLE_HPP
 
@@ -27,4 +10,3 @@ public:
 };
 
 #endif // LOGGABLE_HPP
->>>>>>> d3122ee0e58222ba762f9edf23a88344c9a14b0d
--- src/buildinfo.h
+++ src/buildinfo.h
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-#ifndef BW_BUILDINFO_H
-#define BW_BUILDINFO_H
-
-#include <string_view>
-
-/// @file buildinfo.h
-/// @brief Build information for the application
-/// @namespace bw
-namespace bw
-{
-    /// @brief Build information for the application
-    struct buildinfo
-    {
-        static constexpr std::string_view CommitSHA = "'46bab02'";
-
-        static constexpr std::string_view Timestamp = "2026-02-25T06:42:37";
-        
-        static constexpr std::string_view Version = "0.5.3";
-    };
-
-}
-
-#endif // BW_BUILDINFO_H
-
-=======
 #ifndef BW_BUILDINFO_H
 #define BW_BUILDINFO_H
 
@@ -48,4 +22,3 @@ namespace bw
 
 #endif // BW_BUILDINFO_H
 
->>>>>>> d3122ee0e58222ba762f9edf23a88344c9a14b0d
